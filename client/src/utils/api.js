2cf11c8195fb3ffc64dd8719e3fb60f624ec28bd
Venkata-Narasimhulu <161742696+Venkata-Narasimhulu@users.noreply.github.com>
2025-04-22 22:31:00 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Create an instance of axios with your backend's base URL
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "https://your-render-backend.onrender.com",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://procto-wsgb.onrender.com",
   withCredentials: true, // Optional: only if you're using cookies
 });
 
