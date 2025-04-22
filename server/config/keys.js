7317622af77081c9b3ac9fc19212b0319c2f0ea3
Venkata-Narasimhulu <161742696+Venkata-Narasimhulu@users.noreply.github.com>
2025-04-22 21:26:46 +0530

    Delete server directory

--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://venkatanarasimhulu193:venky@cluster0.qvvuxek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    secretOrKey: "secret",
-  };
