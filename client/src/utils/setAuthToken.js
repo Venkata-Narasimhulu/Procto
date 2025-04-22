1bfa4067cf01c8d4716b4fde5de7204acca70f20
Venkata-Narasimhulu <161742696+Venkata-Narasimhulu@users.noreply.github.com>
2025-04-22 22:31:53 +0530

    Update setAuthToken.js

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,15 +1,13 @@
-import axios from "axios";
+import api from "./api"; // Use the custom axios instance
 
 const setAuthToken = token => {
-
   if (token) {
-    // Apply authorization token to every request if logged in
-    axios.defaults.headers.common["Authorization"] = token;
-  } 
-  else {
-    // Delete auth header
-    delete axios.defaults.headers.common["Authorization"];
+    // Apply authorization token to every request
+    api.defaults.headers.common["Authorization"] = token;
+  } else {
+    // Delete the token from header
+    delete api.defaults.headers.common["Authorization"];
   }
-  
 };
+
 export default setAuthToken;
